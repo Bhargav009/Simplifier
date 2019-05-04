c8eb76be5d2a5b50af1356c0ce0451134c232396
Bhargav Pattem <bhargavpattem@gmail.com>
2019-05-04 19:42:59 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path: '', component:NavbarComponent
+      },
+      {
+        path: 'login', component: LoginComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
